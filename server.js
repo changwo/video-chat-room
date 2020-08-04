fab713928be63f1320c2dc30a3bb494898013617
Habib Kadiri <kadirit@hotmail.com>
2020-08-04 13:01:50 +0200

    inital commit

--- server.js
+++ server.js
@@ -24,6 +24,14 @@ io.on("connection", socket => {
     socket.on("offer", payload => {
         io.to(payload.target).emit("offer", payload);
     });
+
+    socket.on("answer", payload => {
+        io.to(payload.target).emit("answer", payload);
+    })
+
+    socket.on("ice-candidate", incoming => {
+        io.to(incoming.target).emit("ice-candidate", incoming.candidate);
+    })
 })
 
 server.listen(8000, () => console.log('server is running on port 8000'));
