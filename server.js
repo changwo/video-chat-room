5863a8de2d82a1f051a101253ceda56beead738b
Habib Kadiri <kadirit@hotmail.com>
2020-08-04 12:53:37 +0200

    inital commit

--- server.js
+++ server.js
@@ -4,3 +4,22 @@ const app = express();
 const server = http.createServer(app);
 const socket = require("socket.io");
 const io = socket(server);
+
+const rooms = {}
+
+io.on("connection", socket => {
+    socket.on("join room", roomID => {
+        if (rooms[roomID]) {
+            rooms[roomID].push(socket.id);
+        } else {
+            rooms[roomID] = [socket.id]
+        }
+        const otherUser = rooms[roomID].find(id => id !== socket.id)
+        if (otherUser) {
+            socket.emit("other user", otherUser);
+            socket.to(otherUser).emit("user joined", socket.id);
+        }
+    })
+})
+
+server.listen(8000, () => console.log('server is running on port 8000'));
